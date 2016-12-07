51f49344d3f0de541b750f916f73a9d6892ae7cd
Akshay Moghe <akshay@defend7.com>
2016-12-06 20:37:42 -0800

    [frontman] Misc additions to gerrit handlers
    These will be revisited in subsequent commits. WIP

--- frontman/gerrit.go
+++ frontman/gerrit.go
@@ -11,20 +11,22 @@ import (
 
 	"github.com/alioygur/gores"
 	"github.com/amoghe/polly/frontman/datastore"
+	gerrit "github.com/andygrunwald/go-gerrit"
 	"github.com/jinzhu/gorm"
 	"github.com/pkg/errors"
 )
 
 const (
-	localGerritURL = "http://127.0.0.1"
+	localGerritURL = "http://127.0.0.1:10080"
 
 	gerritAdminUsername = "admin"
 	gerritAdminPassword = "supersecret"
 )
 
 type gerritRouter struct {
-	mux *goji.Mux
-	db  *gorm.DB
+	mux            *goji.Mux
+	db             *gorm.DB
+	tokenExtractor TokenExtractor
 }
 
 // OrganizationExposure is how we expose a github organization
@@ -33,13 +35,15 @@ type OrganizationExposure struct {
 }
 
 // NewGerritRouter returns a goji.Mux that handles routes pertaining to Gerrit config
-func NewGerritRouter(db *gorm.DB) http.Handler {
+func NewGerritRouter(db *gorm.DB, te TokenExtractor) http.Handler {
 	g := gerritRouter{
-		mux: goji.SubMux(),
-		db:  db,
+		mux:            goji.SubMux(),
+		db:             db,
+		tokenExtractor: te,
 	}
 	g.mux.HandleFunc(pat.Post("/organizations"), g.ImportOrganization)
 	g.mux.HandleFunc(pat.Post("/organizations/:org_name/repositories"), g.ImportRepository)
+	g.mux.HandleFunc(pat.Get("/servers"), g.ListAvailableServers)
 	return &g
 }
 
@@ -58,12 +62,23 @@ func (g *gerritRouter) ImportOrganization(w http.ResponseWriter, r *http.Request
 	}
 
 	org.Repositories = []datastore.Repository{} // blank out the association field
-	err := datastore.InsertOrganization(g.db, &org.Organization)
+	err := datastore.InsertOrganization(g.db.Debug(), &org.Organization)
 	if err != nil {
 		handleGormError(w, errors.Wrap(err, "failed to insert organization in db"))
 		return
 	}
 
+	// _, err = datastore.GetServerForOrganization(g.db.Debug(), org.Name)
+	// if err == gorm.ErrRecordNotFound {
+	// 	fmt.Fprintln(w, `{"error": "Your organization is not supported yet"}`)
+	// 	return
+	// }
+	// if err != nil {
+	// 	handleGormError(w, errors.Wrap(err, "failed to insert organization in db"))
+	// 	return
+	// }
+
+	log.Println("Created org", org.Name, " using backend server:", org.Server.IPAddr)
 	gores.JSON(w, http.StatusOK, org)
 }
 
@@ -92,27 +107,37 @@ func (g *gerritRouter) ImportRepository(w http.ResponseWriter, r *http.Request) 
 		return
 	}
 
-	log.Println("AAA early return")
-	//
-	// gclt, err := gerrit.NewClient(localGerritURL, nil)
-	// if err != nil {
-	// 	handleError(w, errors.Wrap(err, "failed to setup client to gerrit server"))
-	// 	return
-	// }
-	// gclt.Authentication.SetDigestAuth(gerritAdminUsername, gerritAdminPassword)
-	//
-	// proj, resp, err := gclt.Projects.CreateProject(rep.Name, &gerrit.ProjectInput{
-	// 	Name:              rep.Name,
-	// 	CreateEmptyCommit: false,
-	// })
-	// if err != nil {
-	// 	handleError(w, errors.Wrap(err, "failed to create project in gerrit"))
-	// 	return
-	// }
-	// if resp.StatusCode != http.StatusOK {
-	// 	handleError(w, errors.Errorf("incorrect response code from gerrit (%d)", resp.StatusCode))
-	// 	return
-	// }
-	// log.Println("Created project", proj.Name)
+	log.Println("Setting up gerrit server")
+	gclt, err := gerrit.NewClient(localGerritURL, nil)
+	if err != nil {
+		handleGerritAPIError(w, errors.Wrap(err, "failed to setup client to gerrit server"))
+		return
+	}
+	gclt.Authentication.SetDigestAuth(gerritAdminUsername, gerritAdminPassword)
+
+	proj, resp, err := gclt.Projects.CreateProject(rep.Name, &gerrit.ProjectInput{
+		Name:              rep.Name,
+		CreateEmptyCommit: false,
+	})
+	if err != nil {
+		handleGerritAPIError(w, errors.Wrap(err, "failed to create project in gerrit"))
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		handleGerritAPIError(w, errors.Errorf("incorrect response code from gerrit (%d)", resp.StatusCode))
+		return
+	}
+	token, _ := g.tokenExtractor(r)
+	log.Println("Created project", proj.Name)
+	log.Println("Import the repo using token:", token.AccessToken)
 	gores.JSON(w, http.StatusOK, &rep)
+}
+
+func (g *gerritRouter) ListAvailableServers(w http.ResponseWriter, r *http.Request) {
+	srvs, err := datastore.GetAvailableServer(g.db.Debug())
+	if err != nil {
+		handleGormError(w, err)
+		return
+	}
+	gores.JSONIndent(w, http.StatusOK, srvs, "", "  ")
 }
